6f684875f5c47ee66fa85bb7952076ad37839d3f
Joejoe9204 <50184848+Joejoe9204@users.noreply.github.com>
2020-06-04 12:06:27 -0400

    Add files via upload
    The final version of my senior capstone. Hotseat 2 player game works. The AI has a bit of trouble

---
+++ ChessWithHeight/cmake-build-debug/CMakeFiles/ChessWithHeight.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChessWithHeight.dir/main.c.obj"
+  "ChessWithHeight.exe"
+  "ChessWithHeight.exe.manifest"
+  "ChessWithHeight.lib"
+  "ChessWithHeight.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ChessWithHeight.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
